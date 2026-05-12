1495945d4927534e5848c90dde413f9743851ec1
Guilherme Ganho <guilhermeganho2208@gmail.com>
2026-05-12 15:14:35 -0300

    Initial commit: Projeto ScreenSound com IA segura

---
+++ ScreenSound/Menus/MenuRegistrarBanda.cs
@@ -0,0 +1,39 @@
+﻿using OpenAI.Chat;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuRegistrarBanda : Menu
+{
+    // 1. Criamos um campo para guardar a chave que o Program.cs vai nos dar
+    private string _chave;
+
+    // 2. Criamos um "Construtor": ele obriga quem criar este menu a passar a chave
+    public MenuRegistrarBanda(string chave)
+    {
+        _chave = chave;
+    }
+
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        base.Executar(bandasRegistradas);
+        ExibirTituloDaOpcao("Registro das bandas");
+        Console.Write("Digite o nome da banda que deseja registrar: ");
+
+        string nomeDaBanda = Console.ReadLine()!;
+        Banda banda = new Banda(nomeDaBanda);
+        bandasRegistradas.Add(nomeDaBanda, banda);
+
+        ChatClient client = new ChatClient("gpt-4o-mini", _chave);
+
+        var resposta = client.CompleteChat($"Resuma a banda {nomeDaBanda} em 1 parágrafo. Adote um estilo informal.");
+
+        banda.Resumo = resposta.Value.Content[0].Text;
+
+        Console.WriteLine($"\nA banda {nomeDaBanda} foi registrada com sucesso!");
+
+        Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
---
+++ ScreenSound/Modelos/Banda.cs
@@ -0,0 +1,44 @@
+﻿namespace ScreenSound.Modelos;
+
+internal class Banda : IAvaliavel // no caso de interface a leitura certa fica: Banda implementa a interface IAvaliavel 
+{
+    private List<Album> albuns = new List<Album>();
+    private List<Avaliacao> notas = new List<Avaliacao>();
+
+    public Banda(string nome)
+    {
+        Nome = nome;
+    }
+
+    public string Nome { get; }
+    public double Media
+    {
+        get 
+        {
+            if (notas.Count == 0) return 0;
+            else return notas.Average(a => a.Nota);
+        }
+    }
+
+    public  string? Resumo { get; set; } // para a string aceitar valores nulos.
+    public List<Album> Albuns => albuns;
+
+    public void AdicionarAlbum(Album album) 
+    { 
+        albuns.Add(album);
+    }
+
+    public void AdicionarNota(Avaliacao nota)
+    {
+        notas.Add(nota);
+    }
+
+    public void ExibirDiscografia()
+    {
+        Console.WriteLine($"Discografia da banda {Nome}");
+        foreach (Album album in albuns)
+        {
+            Console.WriteLine($"Álbum: {album.Nome} ({album.DuracaoTotal})");
+        }
+    }
+}
